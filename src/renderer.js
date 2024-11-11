e1a93f1906934bfc88ba07b629d05983ea666f13
秋晚夕落 <qwxl@zero-ai.online>
2024-11-11 20:38:22 +0800

    Add files via upload

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,29 @@
-
-
-
 import { io } from "./socket.io.esm.min.js"
 
 window.io = io
 
-console.log(io)
+/**
+ * Await for socket.io to be loaded
+ * @returns {Promise}
+ */
+function waitingIoLoaded() {
+    return new Promise((resolve, reject) => {
+        executeWhenIoLoaded(resolve)
+    })
+}
+/**
+ * Execute callback when socket.io is loaded
+ * @param {Function} callback 
+ * @param  {...any} args 
+ */
+function executeWhenIoLoaded(callback, ...args) {
+    callback(...args)
+}
+executeWhenIoLoaded()
+
+
+console.log("LiteLoaderQQNT socket.io support loaded!")
+console.log("Variable \"io\" has been loaded into Window")
 
 export {
     io
